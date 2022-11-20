2ef9e5211240a3f1c02846d8717cdc38c2133a04
Muhammad Rizky Millennianno <muhammad.rizky912@ui.ac.id>
2022-11-20 15:40:24 +0700

    minor fix

--- MG811.h
+++ MG811.h
@@ -8,7 +8,7 @@ class MG811{
         MG811(const float& v_res = 5, const int& adc_bit = 8, const int& input = 1, const float& ratio_clean_air = 0.3, const float& ratio_dirty_air = 0.9);
         void init(const int& time_in_ms = 50);
         void set_clean_air(const float& ratio_clean_air, const float& ppm);
-        void set_dirty_air(const float& ratio_dirty_air, const float& ppm)
+        void set_dirty_air(const float& ratio_dirty_air, const float& ppm);
         float raw(const bool& bit = false);         // return raw data (in volt) from the sensor
         void calibrate(const bool& mode = false, const int& duration_in_s = 10, const int& sampling_time_in_s = 1);   // set up the sensor by calibrating two references
         float read();       // return the air quality in 'ppm'
